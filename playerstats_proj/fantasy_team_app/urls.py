6be206d5da5d0de25a73af1d6d2932fdfc39fa90
byeoh100 <rax745@gmail.com>
2024-08-10 19:16:31 -0500

    Create fantasy team views

--- playerstats_proj/fantasy_team_app/urls.py
+++ playerstats_proj/fantasy_team_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import My_fantasy_team
+from .views import My_fantasy_team, My_fantasy_team_position
 
 urlpatterns = [
     path("", My_fantasy_team.as_view(), name='my_fantasy_team'),
+    path("<str:position>/", My_fantasy_team_position.as_view(), name='my_fantasy_team_position')
 ]
--- playerstats_proj/fantasy_team_app/views.py
+++ playerstats_proj/fantasy_team_app/views.py
@@ -13,25 +13,52 @@ from rest_framework.status import HTTP_204_NO_CONTENT, HTTP_400_BAD_REQUEST, HTT
 class My_fantasy_team(TokenReq):
     def get(self, request):
         try:
-            user_fav_team = FantasyTeamSerializer(request.user.fantasy_team, partial=True)
-            return Response(user_fav_team.data, status=HTTP_200_OK)
+            user_fantasy_team = FantasyTeamSerializer(request.user.fantasy_team, partial=True)
+            return Response(user_fantasy_team.data, status=HTTP_200_OK)
         except Exception as e:
             return Response(e, status=HTTP_400_BAD_REQUEST)
     
-    # def put(self, request):
-    #     data = request.data.copy()
-    #     data['user'] = request.user.id
-    #     user_fav_team =  Fantasy_team.objects.get(user=request.user)
-    #     new_user_fav_team = FantasyTeamSerializer(user_fav_team, data=data, partial=True)
-    #     if new_user_fav_team.is_valid():
-    #         new_user_fav_team.save()
-    #         return Response(f'Favorite team changed to {data.get("team")}')
-    #     else:
-    #         return Response(new_user_fav_team.errors, status=HTTP_400_BAD_REQUEST)
+    def delete(self, request):
+        data = request.data.copy()
+        user_fantasy_team = Fantasy_team.objects.get(user=request.user)
+        user_fantasy_team.point_guard = None
+        user_fantasy_team.shooting_guard = None
+        user_fantasy_team.small_forward = None
+        user_fantasy_team.power_forward = None
+        user_fantasy_team.center = None
+        user_fantasy_team.save()
+        return Response(f'Fantasy team deleted', status=HTTP_204_NO_CONTENT)
     
-    # def delete(self, request):
-    #     data = request.data.copy()
-    #     user_fav_team =  Fantasy_team.objects.get(user=request.user)
-    #     user_fav_team.team = None
-    #     user_fav_team.save()
-    #     return Response(f'Favorite team deleted', status=HTTP_204_NO_CONTENT)
+class My_fantasy_team_position(TokenReq):
+    def get(self, request, position):
+        try:
+            user_fantasy_team = Fantasy_team.objects.get(user=request.user)
+            data = {
+                position : getattr(user_fantasy_team, position)
+            }
+            ser_user_fantasy_team = FantasyTeamSerializer(data=data, partial=True)
+            if ser_user_fantasy_team.is_valid():
+                return Response(data, status=HTTP_200_OK)
+            else:
+                return Response(ser_user_fantasy_team.errors, status=HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response(e, status=HTTP_400_BAD_REQUEST)
+
+    def put(self, request, position):
+        try:
+            data = request.data.copy()
+            user_fantasy_team = Fantasy_team.objects.get(user=request.user)
+            ser_user_fantasy_team = FantasyTeamSerializer(user_fantasy_team, data=data, partial=True)
+            if ser_user_fantasy_team.is_valid():
+                ser_user_fantasy_team.save()
+                return Response(ser_user_fantasy_team.data, status=HTTP_200_OK)
+            else:
+                return Response(ser_user_fantasy_team.errors, status=HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response(e, status=HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, position):
+        user_fantasy_team =  Fantasy_team.objects.get(user=request.user)
+        setattr(user_fantasy_team, position, None)
+        user_fantasy_team.save()
+        return Response(f'{position} deleted', status=HTTP_204_NO_CONTENT)
